e1eddce1d13da134a08d09b2ea00890cacf167f1
Moneooo <166874209+Moneooo@users.noreply.github.com>
2025-08-04 08:28:50 -0400

    Updated to 1.21.8

--- src/client/java/org/MoneoDev/CreativeInventoryPlusClient.java
+++ src/client/java/org/MoneoDev/CreativeInventoryPlusClient.java
@@ -1,5 +1,11 @@
 package org.MoneoDev;
 
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.minecraft.item.ItemGroup;
 import org.MoneoDev.config.HotbarMode;
 import org.MoneoDev.hud.HotbarPreviewRenderer;
 import org.MoneoDev.input.InputHandler;
@@ -7,6 +13,19 @@ import net.fabricmc.api.ClientModInitializer;
 
 public class CreativeInventoryPlusClient implements ClientModInitializer {
     private static HotbarMode currentMode = HotbarMode.COPY;
+
+    public static final ItemGroup OPERATOR_TAB = FabricItemGroup.builder()
+            .displayName(Text.literal("Operator Items"))
+            .icon(() -> new ItemStack(Items.COMMAND_BLOCK))
+            .entries((displayContext, entries) -> {
+                entries.add(new ItemStack(Items.COMMAND_BLOCK));
+                entries.add(new ItemStack(Items.STRUCTURE_BLOCK));
+                entries.add(new ItemStack(Items.BARRIER));
+                entries.add(new ItemStack(Items.JIGSAW));
+                entries.add(new ItemStack(Items.LIGHT));
+            })
+            .build();
+
 
     public static HotbarMode getCurrentMode() {
         return currentMode;
